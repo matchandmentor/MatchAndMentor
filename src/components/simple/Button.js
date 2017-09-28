e4b9e210562ff816c661aa0479c20b4214bcf2bb
Unknown <ktucker1024@gmail.com>
2017-09-27 20:12:42 -0500

    added example simple component

---
+++ src/components/simple/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+
+function Button ({onPress, children}) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+export default Button
