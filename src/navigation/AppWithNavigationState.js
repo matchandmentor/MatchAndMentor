8f0445704dfda6f02628f47cd7ecb5d18a2ff1b3
George Spake <gpspake@gmail.com>
2017-10-11 00:00:02 -0500

    eslint style refactor

--- src/navigation/AppWithNavigationState.js
+++ src/navigation/AppWithNavigationState.js
@@ -1,34 +1,35 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { BackHandler } from 'react-native'
-import { addNavigationHelpers, StackNavigator, NavigationActions } from 'react-navigation'
-import Routes from './Routes'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BackHandler } from 'react-native';
+import { addNavigationHelpers, StackNavigator, NavigationActions } from 'react-navigation';
 import PropTypes from 'prop-types';
+import Routes from './Routes';
 
-export const AppNavigator = StackNavigator(Routes)
+export const AppNavigator = StackNavigator(Routes);
 
 class AppWithNavigationState extends Component {
-  componentDidMount () {
-    BackHandler.addEventListener('hardwareBackPress', this.onBackPress)
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
   }
-  componentWillUnmount () {
-    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress)
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
   }
-  onBackPress = () => {
-    const { dispatch, nav } = this.props
+  onBackPress() {
+    const { dispatch, nav } = this.props;
     if (nav.index === 0) {
-      return false
+      return false;
     }
-    dispatch(NavigationActions.back())
-    return true
+    dispatch(NavigationActions.back());
+    return true;
   }
-  render () {
-    const { dispatch, nav } = this.props
+
+  render() {
+    const { dispatch, nav } = this.props;
     return (
       <AppNavigator
         navigation={addNavigationHelpers({ dispatch, state: nav })}
-        />
-    )
+      />
+    );
   }
 }
 
@@ -38,7 +39,7 @@ AppWithNavigationState.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  nav: state.nav
-})
+  nav: state.nav,
+});
 
-export default connect(mapStateToProps)(AppWithNavigationState)
+export default connect(mapStateToProps)(AppWithNavigationState);
