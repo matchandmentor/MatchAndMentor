f5d3ba118b4bd17e96f67ebf841222cb153dd4c2
George Spake <gpspake@gmail.com>
2017-10-25 15:09:04 -0500

    silence timer error

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import AppWithNavigationState from '../navigation/AppWithNavigationState';
 const store = configureStore();
 
 export default class App extends Component {
+  constructor() {
+    super();
+    console.ignoredYellowBox = ['Setting a timer'];
+  }
   componentWillMount() {
     // TODO: remove this when we work on the login story!!!!!!
     auth.signInWithEmailAndPassword('test.user@test.com', 'test123');
