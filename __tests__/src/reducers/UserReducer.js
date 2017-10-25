e920080dbbb6f220cc9f704b657f0479adb46b63
George Spake <gpspake@gmail.com>
2017-10-25 15:03:03 -0500

    add skill reducer tests

--- __tests__/src/reducers/UserReducer.js
+++ __tests__/src/reducers/UserReducer.js
@@ -1,12 +1,14 @@
 /* eslint-env jest */
 import reducer from '../../../src/reducers/UserReducer'
-import { UPDATE_NAME, UPDATE_MENTOR_ROLE, UPDATE_MENTEE_ROLE } from '../../../src/actions/types'
+import { UPDATE_NAME, UPDATE_MENTOR_ROLE, UPDATE_MENTEE_ROLE, UPDATE_SKILL_NAME, UPDATE_SKILL_PROFICIENCY, UPDATE_SKILL } from '../../../src/actions/types'
 
 const INITIAL_STATE = {
   name: '',
   roles: null,
   mentorSelected: false,
   menteeSelected: false,
+  skill: {},
+  skills: [],
 };
 
 test('user reducer should return intial state', () => {
@@ -33,3 +35,31 @@ test('user reducer should handle UPDATE_MENTEE_ROLE', () => {
     menteeSelected: true
   })
 })
+
+test('user reducer should handle UPDATE_SKILL_NAME', () => {
+  expect(
+    reducer(INITIAL_STATE, { type: UPDATE_SKILL_NAME, payload: 'javascript' })
+  ).toEqual({
+    ...INITIAL_STATE,
+    skill: { name: 'javascript' },
+  });
+});
+
+test('user reducer should handle UPDATE_SKILL_PROFICIENCY', () => {
+  expect(
+    reducer(INITIAL_STATE, { type: UPDATE_SKILL_PROFICIENCY, payload: '4' })
+  ).toEqual({
+    ...INITIAL_STATE,
+    skill: { proficiency: '4' },
+  });
+});
+
+test('user reducer should handle UPDATE_SKILL', () => {
+  const skill = { name: 'javascript', proficiency: '5'}
+  expect(
+    reducer(INITIAL_STATE, { type: UPDATE_SKILL, payload: skill })
+  ).toEqual({
+    ...INITIAL_STATE,
+    skills: [skill],
+  });
+});
